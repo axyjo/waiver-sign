c0567925571ed3ebbdba0f9a2a12411b5d1d6a21
Akshay Joshi <me@akshayjoshi.com>
2012-05-15 01:38:45 +0400

    Adding waiver files.

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html><head lang="en-ca"><meta charset="utf-8">
+<title>Resnet - Computer Repair Waiver</title>
+<style>
+@charset "utf-8";@media screen{
+body{margin:0;padding:0;background:#dde7f4;color:#555;font:normal 100.01%/137.5% "Helvetica Neue",Helvetica,Arial,Corbel,sans-serif;}
+hr{clear:both;height:0;margin:1.9em 0 1.8em;padding:0;border:0;border-top:4px solid #f0e2eb;}
+img{margin:0;padding:0;border:0;}
+a,a:link{color:#4d7ec4;text-decoration:underline;}
+a:visited{color:#4d7ec4;text-decoration:underline;}
+a:hover,a:focus,a:active{color:#ea088c;text-decoration:underline;}
+h1{margin:0 0 1em;padding:0;color:#4d7ec4;}
+#content{margin:50px auto 0;padding:2em 1em;width:960px;background-color:#fff;border:1px solid #8f647d;border-bottom-width:2px;border-right-width:2px;}
+}
+</style>
+<style>
+@media print {
+	.sigNav { display: none; }
+}
+	
+@media screen {
+
+.sigPad {
+	margin: 0;
+	padding: 0;
+	width: 400px;
+	}
+	
+.sigPad label {
+	display: block;
+	margin: 0 0 0.515em;
+	padding: 0;
+	
+	color: #000;
+	font: italic normal 1em/1.375 Georgia,Times,sans-serif;
+	}
+	
+.sigPad label.error {
+	color: #f33;
+	}
+	
+.sigPad input {
+	margin: 0;
+	padding: 0.2em 0;
+	width: 396px;
+	
+	border: 1px solid #b0879f;
+	
+	font-size: 1em;
+	}
+	
+.sigPad input.error {
+	border-color: #f33;
+	}
+	
+.sigPad button {
+	margin: 1em 0 0 0;
+	padding: 0.6em 0.6em 0.7em;
+	
+	background-color: #f0e2eb;
+	border: 0;
+	border-radius: 8px;
+	-moz-border-radius: 8px;
+	-webkit-border-radius: 8px;
+	
+	cursor: pointer;
+  
+  width: 396px;
+	
+	color: #4d7ec4;
+	font: bold 1em/1.375 "Helvetica Neue",Helvetica,Arial,sans-serif;
+	text-align: left;
+	}
+	
+.sigPad button:hover {
+	background-color: #4d7ec4;
+	
+	color: #fff;
+	}
+
+.sig {
+	display: none;
+	}
+	
+.sigNav {
+	display: none;
+	height: 2.25em;
+	margin: 0;
+	padding: 0;
+	position: relative;
+
+	list-style-type: none;
+	}
+	
+	.sigNav li {
+		display: inline;
+		float: left;
+		margin: 0;
+		padding: 0;
+		}
+		
+		.sigNav a,
+		.sigNav a:link,
+		.sigNav a:visited {
+			display: block;
+			margin: 0;
+			padding: 0 0.6em;
+			
+			background-color: #fff;
+			
+			color: #4d7ec4;
+			font-weight: bold;
+			line-height: 2.25em;
+			text-decoration: underline;
+			}
+			
+		.sigNav a.current,
+		.sigNav a.current:link,
+		.sigNav a.current:visited {
+			background-color: #4d7ec4;
+			border-top-left-radius: 8px;
+			border-top-right-radius: 8px;
+			-moz-border-radius-topleft: 8px;
+			-moz-border-radius-topright: 8px;
+			-webkit-border-top-left-radius: 8px;
+			-webkit-border-top-right-radius: 8px;
+			
+			color: #fff;
+			text-decoration: none;
+			}
+			
+		.sigNav .typeIt a.current,
+		.sigNav .typeIt a.current:link,
+		.sigNav .typeIt a.current:visited {
+			background-color: #eee;
+			
+			color: #555;
+			}
+			
+		.sigNav .clearButton {
+			bottom: 0.2em;
+			display: none;
+			position: absolute;
+			right: 0;
+			
+			font-size: 0.75em;
+			line-height: 1.375;
+			}
+	
+.sigWrapper {
+	clear: both;
+	height: 110px;
+	
+	border: 1px solid #eee;
+	}
+	
+.sigWrapper.current {
+	border-color: #b0879f;
+	}
+
+.signed .sigWrapper {
+	border: 0;
+	}
+
+.pad {
+	position: relative;
+	}
+	
+	.current .pad {
+		cursor: url("pen.png") 8 8, crosshair;
+		}
+	
+	
+.typeItDesc,
+.drawItDesc {
+	display: none;
+	margin: 0.75em 0 0.515em;
+	padding: 0.515em 0 0;
+	
+	border-top: 3px solid #f0e2eb;
+	
+	color: #000;
+	font: italic normal 1em/1.375 Georgia,Times,sans-serif;
+	}
+	
+p.error {
+	display: block;
+	margin: 0.5em 0;
+	padding: 0.4em;
+	
+	background-color: #f33;
+	
+	color: #fff;
+	font-weight: bold;
+	}
+	
+}
+
+</style>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script></head><body><div id="content">
+<h1>Computer Repair Waiver</h1>
+<p>The technicians of the IS Client Services Department (ResNet) understand that my computer and the information contained therein is valuable and private.
+    Therefore, I can expect to have my equipment and data/information treated with all due care and respect.
+    I understand that ResNet technicians will utilize every reasonable precaution to protect my property,
+    but that any repair process has the potential to create a situation where the operation of the hardware and/or the integrity of the data may become compromised.</p>
+    
+    <p>By signing this agreement, I release the ResNet technicians working on my equipment from any and all damanges that may result from any hardware failure or loss of data.</p>
+    
+    <p>I have reviewed the second page of the waiver for details of service and contract obligations. A copy will be provided for my convenience.
+    The third page of this waiver will be signed by me at the time of the return of my computer.</p>
+    
+    <p>A ResNet technician has fully explained the planned repair to be performed on my computer and will email a written copy of the plan to the address I have provided before performing the repair.
+    If this plan changes in any way for any reason, ResNet will contact me at the supplied email address to verify that I agree to the revised plan.
+    ResNet will not continue to work on my machine until I have agreed to the changes in writing.</p>
+
+<p> THIS VERSION OF THE WAIVER HAS NOT BEEN AUTHORIZED FOR USE YET. </p>
+<form class="sigPad">
+  <label for="name">Print your name</label>
+  <input type="text" name="name" id="name" class="name" /><br />
+
+  <label for="number">Enter your student ID number</label>
+  <input type="number" name="number" id="number" class="number" maxlength="8" /><br />
+
+  <p class="drawItDesc">Draw your signature</p><ul class="sigNav">
+  <li class="drawIt"><a href="#draw-it" >Draw It</a></li>
+  <li class="clearButton"><a href="#clear">Clear</a></li></ul>
+  <div class="sig sigWrapper"><div class="typed"></div>
+  <canvas class="pad" width="396" height="110"></canvas>
+  <input type="hidden" name="output" class="output"></div><br />
+  
+  <label for="rcc_name">RCC/Technical Support Specialist</label>
+  <input type="text" name="rcc_name" id="rcc_name" class="rcc_name" /><br />
+<button onClick="window.print(); return false;">I accept the terms of this agreement.</button></form></div>
+<script src="jquery.signaturepad.min.js"></script>
+<script>
+		$(document).ready(function(){
+			$('.sigPad').signaturePad({drawOnly: true, lineTop: 80});
+		});
+	</script>
+<script src="json2.min.js"></script>
+</body></html>
+
